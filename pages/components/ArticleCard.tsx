58440b54eced8e023fe725611781873a019337cf
victor kiprotich <kosgeivick90@gmail.com>
2024-03-18 13:08:35 +0300

    fix null props

--- pages/components/ArticleCard.tsx
+++ pages/components/ArticleCard.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-type Post = {
+type Post = Partial<{
   id: number;
   title: string;
   image: string;
@@ -9,7 +9,7 @@ type Post = {
   time: string;
   content: string;
   slug: string;
-};
+}>;
 
 type ArticleCardProps = {
   post: Post;
@@ -18,27 +18,27 @@ type ArticleCardProps = {
 const ArticleCard: React.FC<ArticleCardProps> = ({ post }) => {
   return (
     <div className="bg-white rounded shadow-sm overflow-hidden">
-      <Link href={`/posts/${post.id}`}>
+      <Link href={`/posts/${post?.id}`}>
     
           <img
-            src={post.image}
-            alt={post.title}
+            src={post?.image}
+            alt={post?.title}
             className="w-full h-40 object-cover"
           />
      
       </Link>
       <div className="p-4">
-        <Link href={`/posts/${post.id}`}>
+        <Link href={`/posts/${post?.id}`}>
        
-            <h4 className="text-xl font-semibold">{post.title}</h4>
+            <h4 className="text-xl font-semibold">{post?.title}</h4>
         
         </Link>
         <div className="flex justify-between items-center mt-2">
-          <p className="text-gray-500">{post.date}</p>
-          <p className="text-gray-500">{post.time} read</p>
+          <p className="text-gray-500">{post?.date}</p>
+          <p className="text-gray-500">{post?.time} read</p>
         </div>
         <p className="mt-2 text-gray-600">
-          {post.content.substring(0, 100)}...
+          {post?.content?.substring(0, 100)}...
         </p>
       </div>
     </div>
